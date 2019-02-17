d35540035137000f8902036cbaa8f8aa5dd1a5ab
ChinhHo <tuyetchinh1996@mail.com>
2019-02-17 14:10:14 +0700

    Libs Datepicker

--- client/src/services/modules/accountFacebook.service.js
+++ client/src/services/modules/accountFacebook.service.js
@@ -2,6 +2,9 @@ import Api from "@/services";
 
 export default {
   create(cookie, loginType, userId) {
-    return Api().post(`facebook-account?_type=${loginType}&_user=${userId}`, cookie);
+    return Api().post(
+      `facebook-account?_type=${loginType}&_user=${userId}`,
+      cookie
+    );
   }
 };
