bd73488d9543952a093a5182fcc4be023d53e102
thaonam <dinhthao0710@gmail.com>
2019-02-18 22:06:52 +0700

    Responsive Page 404 , 500

--- client/src/views/404/index.vue
+++ client/src/views/404/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="main">
-    <div class="r no_g">
+  <div class="main d_flex justify_content_center align_items_center">
+    <div class="r">
       <div class="c_12 c_md_8 c_xl_8 main--left">
-        <div class="main--text">
-          <h1>404</h1>
-          <p>Oops ! We can't seem to find  the page you're looking for</p>
-          <router-link tag="button" class="btn btn_dark" to="/"
-            >Go back</router-link
-          >
+        <div class="main--text d_flex align_items_center justify_content_center">
+          <div>
+            <h1>404</h1>
+            <p>Oops! We can't seem to find the page you're looking for.</p>
+            <router-link tag="button" class="btn btn_dark" to="/">Go back</router-link>
+          </div>
         </div>
       </div>
-      <div class="d_none d_lg_block c_12 c_md_4 c_xl_4 position_relative">
-        <div class="bg_svg position_absolute">
-          <img :src="`${base}images/svg/chatBee-404.svg`" alt="" />
+      <div class="d_lg_block c_12 c_md_4 c_xl_4">
+        <div class="bg_svg d_flex align_items_center justify_content_center">
+          <img :src="`${base}images/svg/chatBee-404.svg`" alt=""/>
         </div>
       </div>
     </div>
@@ -29,40 +29,6 @@ export default {
 };
 </script>
 
-<style scoped>
-.main {
-  font-family: "Segoe UI Semibold";
-  width: 100%;
-  min-height: 100vh;
-}
-.main--left {
-  width: 100%;
-  min-height: 100vh;
-  position: relative;
-  background-color: #000000;
-}
-.main--text {
-  position: absolute;
-  top: 33%;
-  left: 16%;
-  width: 60%;
-}
-.main--text h1 {
-  font-size: 60px;
-  color: #fff;
-
-}
-.main--text p {
-  font-size: 40px;
-  color: #fff;
-}
-  .main--text button {
-    background: #ffb94a;
-    padding: 8px 30px;
-    text-transform: uppercase;
-  }
-  .bg_svg {
-    top: 31%;
-    left: 35%;
-  }
+<style scoped lang="scss">
+@import "./index.style";
 </style>
