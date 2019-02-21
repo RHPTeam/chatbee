2a21f603c383a09e37fa9ae37ce147d76458193a
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-02-21 18:53:49 +0700

    Update file before merge khanglt branch

--- client/src/views/customer/test/index.vue
+++ client/src/views/customer/test/index.vue
@@ -1,29 +1,38 @@
 <template>
-    <div>
-        <app-test 
-          :data="data"  
-          :currentSort.sync="currentSort" 
-          :currentSortDir.sync="currentSortDir" 
-         
-        />  
-    </div>
+  <div>
+    <app-test
+      :data="data"
+      :currentSort.sync="currentSort"
+      :currentSortDir.sync="currentSortDir"
+    />
+  </div>
 </template>
 
 <script>
-    import AppTest from "@/components/shared/sort";
-    export default {
-        components:{
-            AppTest
-        },
-        data(){
-            return {
-                data: [{"age":2,"breed":"Calico","gender":"male"},{"age":1,"breed":"Siamese","gender":"female"},{"age":6,"breed":"Calico","gender":"female"},{"age":5,"breed":"Bengal","gender":"male"},{"age":2,"breed":"Calico","gender":"female"},{"age":10,"breed":"Bengal","gender":"male"},{"age":4,"breed":"Calico","gender":"female"},{"age":6,"breed":"Siamese","gender":"male"},{"age":1,"breed":"Siamese","gender":"female"},{"age":7,"breed":"Calico","gender":"female"}],
-                currentSort: 'age',
-                currentSortDir: 'asc',
-            }
-        }
-    }
-   
+import AppTest from "@/components/shared/sort";
+export default {
+  components: {
+    AppTest
+  },
+  data() {
+    return {
+      data: [
+        { age: 2, breed: "Calico", gender: "male" },
+        { age: 1, breed: "Siamese", gender: "female" },
+        { age: 6, breed: "Calico", gender: "female" },
+        { age: 5, breed: "Bengal", gender: "male" },
+        { age: 2, breed: "Calico", gender: "female" },
+        { age: 10, breed: "Bengal", gender: "male" },
+        { age: 4, breed: "Calico", gender: "female" },
+        { age: 6, breed: "Siamese", gender: "male" },
+        { age: 1, breed: "Siamese", gender: "female" },
+        { age: 7, breed: "Calico", gender: "female" }
+      ],
+      currentSort: "age",
+      currentSortDir: "asc"
+    };
+  }
+};
 </script>
 
 <style scoped></style>
