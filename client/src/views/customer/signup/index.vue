774e88943f662e434a56da684b937228f8056896
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-02-23 14:57:20 +0700

    Update file finnish fix bug 22022019

--- client/src/views/customer/signup/index.vue
+++ client/src/views/customer/signup/index.vue
@@ -18,6 +18,16 @@
               <icon-logo />
             </icon-base>
           </div>
+          <app-alert
+            :type="
+              this.$store.getters.authStatus == 'error' ? 'alert_danger' : ''
+            "
+            :message="
+              this.$store.getters.authStatus == 'error'
+                ? this.$store.getters.textAuth
+                : ''
+            "
+          />
           <h3 class="title">Register</h3>
           <form @submit.prevent="submit">
             <div
@@ -167,7 +177,6 @@
             <div class="form--action action--register">
               <button
                 type="submit"
-                :disabled="statusForm == false"
                 class="btn btn--login"
               >
                 REGISTER
