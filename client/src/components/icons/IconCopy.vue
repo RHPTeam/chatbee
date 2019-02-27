fda5817682f72d27799a7ea88a187c0753f3f68f
ThaoNam <dinhthao0710@gmail.com>
2019-02-27 18:14:15 +0700

    Add Icon Copy, Link SortDown in page Script Facebook

---
+++ client/src/components/icons/IconCopy.vue
@@ -0,0 +1,18 @@
+<template>
+  <g>
+    <defs>
+      <clipPath id="clip-path">
+        <rect id="Rectangle_1561" data-name="Rectangle 1561" width="26" height="26" transform="translate(1093 266)" fill="#fff" stroke="#707070" stroke-width="1"/>
+      </clipPath>
+    </defs>
+    <g id="ic--copy" transform="translate(-1093 -266)" clip-path="url(#clip-path)">
+      <g id="copy" transform="translate(1093 266)">
+        <g id="Group_209" data-name="Group 209">
+          <path id="Path_428" data-name="Path 428" d="M16.733,4.547H4.646A2.057,2.057,0,0,0,2.59,6.6V23.945A2.057,2.057,0,0,0,4.646,26H16.733a2.057,2.057,0,0,0,2.055-2.055V6.6A2.063,2.063,0,0,0,16.733,4.547Zm.612,19.392a.618.618,0,0,1-.618.618H4.64a.618.618,0,0,1-.618-.618V6.6a.618.618,0,0,1,.618-.618H16.727a.618.618,0,0,1,.618.618Z" fill="#999"/>
+          <path id="Path_429" data-name="Path 429" d="M21.354,0H9.267A2.057,2.057,0,0,0,7.212,2.055a.719.719,0,1,0,1.438,0,.618.618,0,0,1,.618-.618H21.354a.618.618,0,0,1,.618.618V19.4a.618.618,0,0,1-.618.618.719.719,0,0,0,0,1.438A2.057,2.057,0,0,0,23.41,19.4V2.055A2.057,2.057,0,0,0,21.354,0Z" fill="#999"/>
+        </g>
+      </g>
+    </g>
+  </g>
+</template>
+
