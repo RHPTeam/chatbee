286565c88e9307ebf311db302f599b23e4d3ecad
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-03-01 16:50:22 +0700

    Update file before merge nam branch

--- client/src/components/layouts/header.vue
+++ client/src/components/layouts/header.vue
@@ -49,7 +49,7 @@
             <p class=" mb_0">varun@gmail.com</p>
           </div>
         </div>
-        <a class="dropdown--item" href="javascript:void(0)">
+        <router-link class="dropdown--item" :to="{ name: 'c_account' }">
           <icon-base
             icon-name="account"
             width="18"
@@ -58,7 +58,7 @@
           >
             <icon-account />
           </icon-base>
-          Thiết lập tài khoản</a
+          Thiết lập tài khoản</router-link
         >
         <a class="dropdown--item" href="javascript:void(0)">
           <icon-base
