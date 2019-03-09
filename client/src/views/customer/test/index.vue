a1196a9f58e698e40d91622045edaed7a5493b84
lnguyen24794 <lamnt@fasttrack.edu.vn>
2019-03-09 16:03:11 +0700

    finish draggable

--- client/src/views/customer/test/index.vue
+++ client/src/views/customer/test/index.vue
@@ -1,22 +1,24 @@
 <template>
- <div id='app'>
-  <ul class="todo-list">
-    <li @dragover.prevent @drop="dragFinish(-1, $event)" v-if="dragging > -1" class="trash-drop todo-item" v-bind:class="{drag: isDragging}">Delete</li>
-    
-    <li v-else>
-      <input placeholder="Type new task and press enter" type="text" class="new-todo todo-item" v-model="newItem" @keyup.enter="addItem">
-    </li>
-    
-    <li class="todo-item" v-for="(item,i) in todos" :key="item.id" draggable="true" @dragstart="dragStart(i, $event)" @dragover.prevent @dragenter="dragEnter" @dragleave="dragLeave" @dragend="dragEnd" @drop="dragFinish(i, $event)">
-      <input type="checkbox" v-model="item.done" />
-      <span :class="{done: item.done}">{{ item.title }}</span>
-      <span class="remove-item" @click="removeItem(item)">x</span>
-    </li>
-  </ul>
-</div>
+  <div id='app'>
+    <app-dragg :works="data"/>
+  </div>
 </template>
-<script src="./index.script.js"></script>
-
-<style scoped lang="scss">
-@import "./index.style";
-</style>
+<script >
+ import AppDragg from "@/components/shared/draggable_library";
+ export default {
+   components:{
+     AppDragg
+   },
+   data(){
+     return {
+       data:[
+         {
+           id:1,
+           title:"ABC",
+           done:true
+         }
+       ]
+     } 
+   }
+ }
+</script>
