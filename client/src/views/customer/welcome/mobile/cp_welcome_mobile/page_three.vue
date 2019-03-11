106c9783f9fbf94a8e6f8a016e5acac3df031404
khangle <khangle@under2.com>
2019-03-11 22:49:50 +0700

    update file before merge sourcess branch dev-nam-vue

--- client/src/views/customer/welcome/mobile/cp_welcome_mobile/page_three.vue
+++ client/src/views/customer/welcome/mobile/cp_welcome_mobile/page_three.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popup--content position_fixed" :data-theme="currentTheme">
+  <div class="popup--content position_fixed">
     <div class="popup position_relative">
       <!--Start: Popup Header-->
       <div class="popup--wrapper">
@@ -13,63 +13,75 @@
       <!--Start: Popup Body-->
       <div class="popup--body">
         <div
-                class="main--content d_flex flex_column justify_content_center align_items_center p_4"
+                class="main--content d_flex flex_column justify_content_center align_items_center"
         >
           <div class="img--content"></div>
-          <div class="main--content-title">Tự động trả lời</div>
+          <div class="main--content-title">Trò chuyện dễ dàng</div>
           <div class="main--content-sub mt_4 mb_4">Cho phép thiết lập tin nhắn trả lời tự động theo kịch bản.
             <br/>
             Thiết lập trả lời tin nhắn tự động theo kịch bản.
           </div>
-          <input type="button" class="main--content-button mb_4" value="Tiếp Theo">
+          <input @click="isPageFour = true" type="button" class="main--content-button" value="Tiếp Theo">
+          <input @click="isPageTwo = true" type="button" class="main--content-button-back" value="TRƯỚC">
         </div>
       </div>
       <!--End: Popup Body-->
 
       <!--Popup Page 2-->
       <transition name="changeTheme">
-        <app-change-theme
-                v-if="isChangeTheme == true"
+        <app-page-two
+                v-if="isPageTwo == true"
                 :data-theme="currentTheme"
-                :popupTheme="isChangeTheme"
-                @closeChangeTheme="isChangeTheme = $event"
+                :popupTheme="isPageTwo"
+                @closePageTwo="isPageTwo = $event"
         />
       </transition>
       <!--End: Popup Page 2-->
 
-      <!--Popup Page 3-->
-      <transition name="changeTheme">
-        <app-change-password
-                v-if="isChangePassword == true"
-                :data-theme="currentTheme"
-                :popupPassword="isChangePassword"
-                @closeChangePassword="isChangePassword = $event"
-        />
-      </transition>
-      <!--End: Popup Page 3-->
-
       <!--Popup Page 4-->
       <transition name="changeTheme">
-        <app-change-password
-                v-if="isChangePassword == true"
+        <app-page-four
+                v-if="isPageFour == true"
                 :data-theme="currentTheme"
-                :popupPassword="isChangePassword"
-                @closeChangePassword="isChangePassword = $event"
+                :popupPassword="isPageFour"
+                @closePageFour="isPageFour = $event"
         />
       </transition>
       <!--End: Popup Page 4-->
+
     </div>
   </div>
 </template>
+
 <script>
+    import AppPageFour from "./page_four";
+    import AppPageTwo from "./page_two";
+
     export default {
+        props: ["isPageThree"],
+
+        data() {
+            return {
+                isPageFour: false,
+                isPageTwo: false,
+            }
+        },
         computed: {
             currentTheme() {
                 return this.$store.getters.themeName;
             }
         },
-    }
+        components: {
+            AppPageFour,
+            AppPageTwo
+        },
+        methods: {
+            closePageThree: function() {
+                this.$emit("closePageThree", false);
+            }
+        }
+    };
 </script>
 <style lang="scss" scoped>
-  @import "./index_mobile.style";
+  @import "page_three.style";
 </style>
--- client/src/views/customer/welcome/mobile/cp_welcome_mobile/page_two.vue
+++ client/src/views/customer/welcome/mobile/cp_welcome_mobile/page_two.vue
@@ -13,15 +13,16 @@
       <!--Start: Popup Body-->
       <div class="popup--body">
         <div
-                class="main--content d_flex flex_column justify_content_center align_items_center p_4"
+                class="main--content d_flex flex_column justify_content_center align_items_center"
         >
           <div class="img--content"></div>
-          <div class="main--content-title">Tự động trả lời</div>
+          <div class="main--content-title">Chiến dịch truyền thông</div>
           <div class="main--content-sub mt_4 mb_4">Cho phép thiết lập tin nhắn trả lời tự động theo kịch bản.
             <br/>
             Thiết lập trả lời tin nhắn tự động theo kịch bản.
           </div>
-          <input type="button" class="main--content-button mb_4" value="Tiếp Theo">
+          <input @click="isPageThree = true" type="button" class="main--content-button" value="Tiếp Theo">
+          <input type="button" class="main--content-button-back" value="TRƯỚC">
         </div>
       </div>
       <!--End: Popup Body-->
@@ -29,39 +30,44 @@
 
       <!--Popup Page 3-->
       <transition name="changeTheme">
-        <app-change-password
-                v-if="isChangePassword == true"
+        <app-page-three
+                v-if="isPageThree == true"
                 :data-theme="currentTheme"
-                :popupPassword="isChangePassword"
-                @closeChangePassword="isChangePassword = $event"
+                :popupPassword="isPageThree"
+                @closePageThree="isPageThree = $event"
         />
       </transition>
       <!--End: Popup Page 3-->
 
-      <!--Popup Page 4-->
-      <transition name="changeTheme">
-        <app-change-password
-                v-if="isChangePassword == true"
-                :data-theme="currentTheme"
-                :popupPassword="isChangePassword"
-                @closeChangePassword="isChangePassword = $event"
-        />
-      </transition>
-      <!--End: Popup Page 4-->
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        props: ["isPageTwo"],
-        methods: {
-            closePageTwo: function() {
-                this.$emit("closePageTwo", false);
-            }
-        }
-    };
+import AppPageThree from "./page_three";
+
+export default {
+  props: ["isPageTwo"],
+  data() {
+      return {
+          isPageThree: false,
+      }
+  },
+  computed: {
+    currentTheme() {
+      return this.$store.getters.themeName;
+    }
+  },
+  components: {
+      AppPageThree
+  },
+  methods: {
+    closePageTwo: function() {
+      this.$emit("closePageTwo", false);
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-  @import "page_two.style";
+@import "page_two.style";
 </style>
