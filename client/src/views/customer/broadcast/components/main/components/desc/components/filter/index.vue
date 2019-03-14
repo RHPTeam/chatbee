9bf404211071ff3214f70eff75547cc83d943f3a
khangle <khangle@under2.com>
2019-03-14 19:01:29 +0700

    update file then config layout broadcast on 140319

--- client/src/views/customer/broadcast/components/main/components/desc/components/filter/index.vue
+++ client/src/views/customer/broadcast/components/main/components/desc/components/filter/index.vue
@@ -1,34 +1,74 @@
 <template>
   <div class="filter p_3 mb_3">
-    <div class="filter--action">
-      Filter action
+    <div class="filter--action d_flex align_items_center justify_content_between">
+      <!--Start: component filter-->
+      <div class="left">
+        <app-filter/>
+      </div>
+      <!--End: component filter-->
+      <!--Start: Filter icon-->
+      <div class="filter--icon d_flex ml_auto">
+        <div class="filter--icon-plus text_center">
+          <icon-base
+            class="icon-plus"
+            icon-name="plus"
+            width="20"
+            height="20"
+            viewBox="0 0 63 63"
+          >
+            <icon-plus />
+          </icon-base>
+        </div>
+        <div class="filter--icon-remove">
+          <icon-base
+            class="icon-remove"
+            icon-name="remove"
+            width="20"
+            height="20"
+            viewBox="0 0 18 18"
+          >
+            <icon-remove />
+          </icon-base>
+        </div>
+      </div>
+      <!--End: Filter icon-->
     </div>
-    <div class="filter--result">
-      Tìm thấy 50 người khả dụng
+    <!--Start: Result filter-->
+    <div class="filter--result text_left mt_4">
+      <div>Tìm thấy 50 người khả dụng</div>
+      <div class="option--user-list d_flex align_items_center mt_3">
+        <div class="option--user-item item mr_2">
+          <!--use backgroung images-->
+        </div>
+        <div class="option--user-more item text_center ml_auto">
+          +110
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
+<script>
+  import AppFilter from "@/components/shared/filter";
+export default {
+  data() {
+    return {
+      showFilterAttribute: false,
+      showFilterOption: false
+    };
+  },
+  methods: {
+    closeFilterAttribute(){
+      this.showFilterAttribute =false;
+    },
+    closeFilterOption(){
+      this.showFilterOption =false;
+    }
+  },
+  components: {
+    AppFilter
+  }
+};
+</script>
 <style lang="scss" scoped>
-/************* CUSTOM CSS COMPONENT ****************/
-/**
-* NOTE: U have to code scss with 3 level which mean is 3 indent
-*/
-
-.filter {
-  background-color: #fff;
-  border: 1px solid #ebebeb;
-}
-
-/************* CUSTOM CSS THEME ****************/
-
-/******** 01. Theme Light *********/
-
-div[data-theme="light"] .filter {
-}
-
-/******** 02. Theme Dark *********/
-
-div[data-theme="dark"] .filter {
-}
+@import "./index.style";
 </style>
