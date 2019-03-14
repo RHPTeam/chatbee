8b3b502cb7c2b7025bd6896e7934ef5f9108b787
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-03-14 11:28:17 +0700

    Update structure for broadcast

--- client/src/views/customer/broadcast/components/main/index.script.js
+++ client/src/views/customer/broadcast/components/main/index.script.js
@@ -1,23 +1,12 @@
-import IconBase from "@/components/icons/IconBase";
-import IconRemove from "@/components/icons/IconRemove";
-import IconCopy from "@/components/icons/IconCopy";
-import IconLink from "@/components/icons/IconLink";
-import IconImage from "@/components/icons/IconImage";
-import IconText from "@/components/icons/IconText";
-import IconPlus from "@/components/icons/IconPlus";
-import IconCancel from "@/components/icons/IconCancel";
-import IconSandClock from "@/components/icons/IconSandClock";
-import IconTag from "@/components/icons/IconTag";
-import IconMove from "@/components/icons/IconMove";
-import IconUploadImage from "@/components/icons/IconUploadImage";
-import IconDropDown from "@/components/icons/IconDropDown";
-import IconPlayButton from "@/components/icons/IconPlaysButton";
 import DatePicker from "@/components/shared/datepicker_library";
 import OptionTimes from "./option_time_broadcast";
 import AddElement from "./add_element";
 import PopupDelete from "@/components/popupDelete/popup_delete";
 
-import AppHeader from "./components/header"
+import AppHeader from "./components/header";
+import AppDesc from "./components/desc";
+import AppBody from "./components/body";
+import AppFooter from "./components/footer";
 
 export default {
   data() {
@@ -34,24 +23,13 @@ export default {
     }
   },
   components: {
-    IconBase,
-    IconCopy,
-    IconLink,
-    IconRemove,
-    IconImage,
-    IconText,
-    IconCancel,
-    IconSandClock,
-    IconTag,
-    IconMove,
-    IconUploadImage,
-    IconPlus,
-    IconDropDown,
-    IconPlayButton,
     DatePicker,
     OptionTimes,
     AddElement,
     PopupDelete,
-    AppHeader
+    AppHeader,
+    AppDesc,
+    AppBody,
+    AppFooter
   }
 };
