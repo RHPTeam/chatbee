59afeae98d5b0b1584865e911f498cff28414d53
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-03-15 10:48:58 +0700

    Update file before move code to server

--- client/src/views/customer/broadcast/components/main/default/index.vue
+++ client/src/views/customer/broadcast/components/main/default/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main p_3">
+  <div class="main p_3" :data-theme="themeName">
     <div class="null--container d_flex">
       <span class="null--image mr_3 mt_2">
         <img :src="nullImage" alt="Null Image" />
@@ -16,6 +16,11 @@
 
 <script>
 export default {
+  computed: {
+    themeName() {
+      return this.$store.getters.themeName;
+    }
+  },
   data() {
     return {
       nullImage: require("@/assets/images/broadcast_null.png")
@@ -25,22 +30,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+/************* CUSTOM CSS COMPONENT ****************/
+/**
+  * NOTE: U have to code scss with 3 level which mean is 3 indent
+   */
+
 .null--container {
   width: 60%;
   border-radius: 7px;
-  background-color: #27292c;
   padding: 32px;
   margin: 0 auto;
   position: relative;
+
   .null--image {
     img {
       height: 48px;
       width: 48px;
     }
   }
+
   .null--text {
     line-height: 2;
     text-align: left;
+  }
+}
+
+/************* CUSTOM CSS THEME ****************/
+
+/******** 01. Theme Light *********/
+div[data-theme="light"] {
+  .null--container {
+    background-color: rgba(225, 225, 225, 0.3);
+  }
+}
+
+/******** 02. Theme Dark *********/
+
+div[data-theme="dark"] {
+  .null--container {
+    background-color: #27292c;
   }
 }
 </style>
