653680e5ef3cd90e44111bc316b79f8b399a84b8
ChinhHo <tuyetchinh1996@mail.com>
2019-03-16 00:38:03 +0700

    Update style for Account

--- server/src/controllers/account.controller.js
+++ server/src/controllers/account.controller.js
@@ -20,6 +20,7 @@ const BroadCast = require('../models/Broadcasts.model')
 
 const JsonResponse = require('../configs/res')
 const checkPhone = require('../helpers/util/checkPhone.util')
+const Secure = require('../helpers/util/secure.util')
     // set one cookie
 const option = {
     maxAge: 1000 * 60 * 60 * 24, // would expire after 1 days
@@ -40,127 +41,135 @@ const signToken = user => {
 }
 
 module.exports = {
-  /**
-   * Register By User
-   * @param req
-   * @param res
-   */
-  signUp: async (req, res) => {
-    let role = ""
-    const { email, phone } = req.value.body
-    const isPhone = checkPhone(req.value.body.phone)
-    if (isPhone === false) {
-      return res
-        .status(403)
-        .json(JsonResponse('Number phone is not correctly!', null))
-    }
-    const foundUserEmail = await Account.findOne({
-      email
-    })
-    if (foundUserEmail) {
-      return res.status(404).json(JsonResponse('Email is exists!', null))
-    }
-    const foundUserPhone = await Account.findOne({
-      phone
-    })
-    if (foundUserPhone) {
-      return res.status(404).json(JsonResponse('Number phone is exists!', null))
-    }
-    const objDefine = {
-      email: req.value.body.email,
-      name: req.value.body.name,
-      phone: req.value.body.phone,
-      password: req.value.body.password,
-      imageAvatar: req.body.imageAvatar ? base64Img.base64Sync(req.value.body.imageAvatar) : ''
-    }
-    const newUser = await new Account(objDefine)
-    const sessionToken = await signToken(newUser)
-    await res.cookie('sid', sessionToken, option)
-    await res.cookie('uid', newUser._id, option)
-    const expireDate = new Date(newUser.created_at)
-    newUser.expireDate = expireDate.setDate(expireDate.getDate() + 3)
-    await newUser.save()
-    newUser._role.toString() === '5c6a59f61b43a13350fe65d8' ? res.cookie('c_fr', 0 ,option) : newUser._role.toString() === '5c6a598f1b43a13350fe65d6' ? res.cookie('c_fr', 1 ,option) : newUser._role.toString()  === '5c6a57e7f02beb3b70e7dce0'? res.cookie('c_fr', 2 ,option) : res.status(405).json(JsonResponse('You are not assign!', null))
+    /**
+     * Register By User
+     * @param req
+     * @param res
+     */
+    signUp: async(req, res) => {
+        let role = ""
+        const {
+            email,
+            phone
+        } = req.value.body
+        const isPhone = checkPhone(req.value.body.phone)
+        if (isPhone === false) {
+            return res
+                .status(403)
+                .json(JsonResponse('Number phone is not correctly!', null))
+        }
+        const foundUserEmail = await Account.findOne({
+            email
+        })
+        if (foundUserEmail) {
+            return res.status(404).json(JsonResponse('Email is exists!', null))
+        }
+        const foundUserPhone = await Account.findOne({
+            phone
+        })
+        if (foundUserPhone) {
+            return res.status(404).json(JsonResponse('Number phone is exists!', null))
+        }
+        const objDefine = {
+            email: req.value.body.email,
+            name: req.value.body.name,
+            phone: req.value.body.phone,
+            password: req.value.body.password,
+            imageAvatar: req.body.imageAvatar ? base64Img.base64Sync(req.value.body.imageAvatar) : ''
+        }
+        const newUser = await new Account(objDefine)
+        const sessionToken = await signToken(newUser)
+        await res.cookie('sid', sessionToken, option)
+        await res.cookie('uid', newUser._id, option)
+        const expireDate = new Date(newUser.created_at)
+        newUser.expireDate = expireDate.setDate(expireDate.getDate() + 3)
+        await newUser.save()
+        newUser._role.toString() === '5c6a59f61b43a13350fe65d8' ? res.cookie('c_fr', 0, option) : newUser._role.toString() === '5c6a598f1b43a13350fe65d6' ? res.cookie('c_fr', 1, option) : newUser._role.toString() === '5c6a57e7f02beb3b70e7dce0' ? res.cookie('c_fr', 2, option) : res.status(405).json(JsonResponse('You are not assign!', null))
 
-    // create group default when signup
-    const defaultGroup = await new GroupBlock()
-    defaultGroup.name = 'Mặc Định'
-    defaultGroup._account = newUser._id
-    await defaultGroup.save()
+        // create group default when signup
+        const defaultGroup = await new GroupBlock()
+        defaultGroup.name = 'Mặc Định'
+        defaultGroup._account = newUser._id
+        await defaultGroup.save()
 
-    // create block welcome in default
-    const  defaultBlock = await  new Block()
-    defaultBlock.name = 'Welcome'
-    defaultBlock._account = newUser._id
-    defaultBlock._groupBlock = defaultGroup._id
-    await defaultBlock.save()
-    defaultGroup.blocks.push(defaultBlock._id)
-    await defaultGroup.save()
+        // create block welcome in default
+        const defaultBlock = await new Block()
+        defaultBlock.name = 'Welcome'
+        defaultBlock._account = newUser._id
+        defaultBlock._groupBlock = defaultGroup._id
+        await defaultBlock.save()
+        defaultGroup.blocks.push(defaultBlock._id)
+        await defaultGroup.save()
 
-    // Create block default in broadcast type schedule
-    const defaultSchedule = await new BroadCast()
-    defaultSchedule.typeBroadCast = 'Thiết lập bộ hẹn'
-    defaultSchedule._account = newUser._id
-    await defaultSchedule.save()
-    const defaultBlockSchedule = await new Block()
-    const date = new Date()
-    date.setHours(12,0,0)
-    date.setDate(date.getDate()+1)
-    defaultBlockSchedule.name = date.toString()
-    defaultBlockSchedule._account = newUser._id
-    await defaultBlockSchedule.save()
-    defaultSchedule.blocks.push({blockId:defaultBlockSchedule._id})
-    await defaultSchedule.save()
+        // Create block default in broadcast type schedule
+        const defaultSchedule = await new BroadCast()
+        defaultSchedule.typeBroadCast = 'Thiết lập bộ hẹn'
+        defaultSchedule._account = newUser._id
+        await defaultSchedule.save()
+        const defaultBlockSchedule = await new Block()
+        const date = new Date()
+        date.setHours(12, 0, 0)
+        date.setDate(date.getDate() + 1)
+        defaultBlockSchedule.name = date.toString()
+        defaultBlockSchedule._account = newUser._id
+        await defaultBlockSchedule.save()
+        defaultSchedule.blocks.push({
+            blockId: defaultBlockSchedule._id
+        })
+        await defaultSchedule.save()
 
-    // Add cfr to data storage of browser
-    if (newUser._role.toString() === '5c6a59f61b43a13350fe65d8') {
-      role = randomstring.generate(10) + 0 + randomstring.generate(1997)
-    } else if (newUser._role.toString() === '5c6a598f1b43a13350fe65d6') {
-      role = randomstring.generate(10) + 1 + randomstring.generate(1997)
-    } else if (newUser._role.toString() === '5c6a57e7f02beb3b70e7dce0') {
-      role = randomstring.generate(10) + 1 + randomstring.generate(1997)
-    }
-    res.status(200).json(
-      JsonResponse('Successfully!', {
-        _id: newUser._id,
-        email: newUser.email,
-        token: sessionToken,
-        role: role
-      })
-    )
-  },
+        // Add cfr to data storage of browser
+        if (newUser._role.toString() === '5c6a59f61b43a13350fe65d8') {
+            role = randomstring.generate(10) + 0 + randomstring.generate(1997)
+        } else if (newUser._role.toString() === '5c6a598f1b43a13350fe65d6') {
+            role = randomstring.generate(10) + 1 + randomstring.generate(1997)
+        } else if (newUser._role.toString() === '5c6a57e7f02beb3b70e7dce0') {
+            role = randomstring.generate(10) + 1 + randomstring.generate(1997)
+        }
+        res.status(200).json(
+            JsonResponse('Successfully!', {
+                _id: newUser._id,
+                email: newUser.email,
+                token: sessionToken,
+                role: role
+            })
+        )
+    },
 
-  /**
-   * Login Local Using Passport Middleware By User
-   * @param req
-   * @param res
-   */
-  signIn: async (req, res) => {
-    let role = ""
-    const foundUser = await Account.findById(req.user._id).select('-password')
-    // check expire date
-    if (Date.now() >= (foundUser.expireDate).getTime()) return res.status(405).json(JsonResponse('Account expire, please buy license to continue', { token: [], user: null }))
+    /**
+     * Login Local Using Passport Middleware By User
+     * @param req
+     * @param res
+     */
+    signIn: async(req, res) => {
+        let role = ""
+        const foundUser = await Account.findById(req.user._id).select('-password')
+            // check expire date
+        if (Date.now() >= (foundUser.expireDate).getTime()) return res.status(405).json(JsonResponse('Account expire, please buy license to continue', {
+            token: [],
+            user: null
+        }))
 
-    // Generate the token
-    const sessionToken = await signToken(req.user)
-    res.cookie('sid', sessionToken, option)
-    res.cookie('uid', foundUser._id.toString(), option)
-    foundUser._role.toString() === '5c6a59f61b43a13350fe65d8' ? res.cookie('c_fr', 0) : foundUser._role.toString() === '5c6a598f1b43a13350fe65d6' ? res.cookie('c_fr', 1 ,option) : foundUser._role.toString() === '5c6a57e7f02beb3b70e7dce0' ? res.cookie('c_fr', 2 ,option) : res.status(405).json(JsonResponse('You are not assign!', null))
-    if (foundUser._role.toString() === '5c6a59f61b43a13350fe65d8') {
-      role = randomstring.generate(10) + 0 + randomstring.generate(1997)
-    } else if (foundUser._role.toString() === '5c6a598f1b43a13350fe65d6') {
-      role = randomstring.generate(10) + 1 + randomstring.generate(1997)
-    } else if (foundUser._role.toString() === '5c6a57e7f02beb3b70e7dce0') {
-      role = randomstring.generate(10) + 1 + randomstring.generate(1997)
-    }
-    res.status(200).json(
-      JsonResponse('Successfully!', {
-        token: sessionToken,
-        user: foundUser,
-        role: role
-      })
-    )
-  },
+        // Generate the token
+        const sessionToken = await signToken(req.user)
+        res.cookie('sid', sessionToken, option)
+        res.cookie('uid', foundUser._id.toString(), option)
+        foundUser._role.toString() === '5c6a59f61b43a13350fe65d8' ? res.cookie('c_fr', 0) : foundUser._role.toString() === '5c6a598f1b43a13350fe65d6' ? res.cookie('c_fr', 1, option) : foundUser._role.toString() === '5c6a57e7f02beb3b70e7dce0' ? res.cookie('c_fr', 2, option) : res.status(405).json(JsonResponse('You are not assign!', null))
+        if (foundUser._role.toString() === '5c6a59f61b43a13350fe65d8') {
+            role = randomstring.generate(10) + 0 + randomstring.generate(1997)
+        } else if (foundUser._role.toString() === '5c6a598f1b43a13350fe65d6') {
+            role = randomstring.generate(10) + 1 + randomstring.generate(1997)
+        } else if (foundUser._role.toString() === '5c6a57e7f02beb3b70e7dce0') {
+            role = randomstring.generate(10) + 1 + randomstring.generate(1997)
+        }
+        res.status(200).json(
+            JsonResponse('Successfully!', {
+                token: sessionToken,
+                user: foundUser,
+                role: role
+            })
+        )
+    },
 
     /**
      * Get User (Query can get one data)
@@ -186,26 +195,29 @@ module.exports = {
      * @param res
      */
     update: async(req, res) => {
-        const { body, query } = req
-        if (!query._userId) {
+        const authorization = req.headers.authorization
+        const userId = Secure(res, authorization)
+        const {
+            body
+        } = req
+        let objectDefine = req.body
+        objectDefine.imageAvatar = base64Img.base64Sync(req.body.imageAvatar)
+        if (!userId) {
             return res.status(405).json(JsonResponse('Not authorized!', null))
         }
-        const foundUser = await Account.findById(query._userId)
+        const foundUser = await Account.findById(userId)
         if (!foundUser) {
             return res.status(403).json(JsonResponse('User is not found!', null))
         }
-        if (foundUser._role.level.toString().toLowerCase() === "member" && body._role) {
-            return res.status(405).json(JsonResponse('Bạn không có quyền cho tính năng này!', null))
-        }
-        if (JSON.stringify(query._userId) !== JSON.stringify(foundUser._id)) {
+        if (JSON.stringify(userId) !== JSON.stringify(foundUser._id)) {
             return res.status(403).json(JsonResponse('Authorized is wrong!', null))
         }
         if (body.password) {
             return res.status(403).json(JsonResponse('Có lỗi xảy ra! Vui lòng kiểm tra lại API!', null))
         }
         const dataUserUpdated = await Account.findByIdAndUpdate(
-            query._userId, {
-                $set: body
+            userId, {
+                $set: objectDefine
             }, {
                 new: true
             }
@@ -221,7 +233,9 @@ module.exports = {
      * @param res
      */
     deleteUser: async(req, res) => {
-        const { query } = req
+        const {
+            query
+        } = req
         const userId = query._userId
         const foundUser = await Account.findById(userId)
         if (!foundUser) {
@@ -237,7 +251,10 @@ module.exports = {
      * @param res
      */
     changePassword: async(req, res) => {
-        const { body, query } = req
+        const {
+            body,
+            query
+        } = req
         if (!query._userId) {
             return res.status(405).json(JsonResponse('Not authorized!', null))
         }
@@ -263,7 +280,10 @@ module.exports = {
      * @param res
      */
     createNewPassword: async(req, res) => {
-        const { body, query } = req
+        const {
+            body,
+            query
+        } = req
         if (!query._userId) {
             return res.status(405).json(JsonResponse('Not authorized!', null))
         }
@@ -299,9 +319,13 @@ module.exports = {
      * @param next
      */
     resetPassword: async(req, res, next) => {
-        const { email } = req.body
+        const {
+            email
+        } = req.body
         if (!email) return res.status(405).json(JsonResponse('Not email!', null))
-        const foundUser = await Account.findOne({ email })
+        const foundUser = await Account.findOne({
+            email
+        })
         if (!foundUser) {
             return res.status(405).json(JsonResponse('Not found User!', null))
         }
@@ -338,7 +362,11 @@ module.exports = {
                 if (err) return next(err)
             }
         )
-        const updateUser = await Account.findOneAndUpdate({ email }, { code: code })
+        const updateUser = await Account.findOneAndUpdate({
+            email
+        }, {
+            code: code
+        })
         if (!updateUser) {
             return res.status(405).json(JsonResponse('Update false!', null))
         }
@@ -370,11 +398,16 @@ module.exports = {
      * @param next
      */
     checkCode: async(req, res, next) => {
-        const { email, code } = req.body
+        const {
+            email,
+            code
+        } = req.body
         if (!email || !code) {
             return res.status(405).json(JsonResponse('Not email or not code!', null))
         }
-        const foundUser = await Account.findOne({ email })
+        const foundUser = await Account.findOne({
+            email
+        })
         if (!foundUser) {
             return res.status(405).json(JsonResponse('Not found User!', null))
         }
