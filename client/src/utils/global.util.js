8fee4b8e3d5bad980db002641c7e3035f77f0ecc
Yến Đặng <dangthiyen103@gmail.com>
2019-03-19 15:37:42 +0700

    Fix Icon on Sidebar & Fix Avatar Dropdown menu

--- client/src/utils/global.util.js
+++ client/src/utils/global.util.js
@@ -7,6 +7,7 @@ import IconAddUser from "@/components/icons/IconAddUser";
 import IconArrowDown from "@/components/icons/IconArrowDown";
 import IconAutoAnswer from "@/components/icons/IconAutoAnswer";
 import IconAutoReply from "@/components/icons/IconAutoReply";
+import IconBroadcast from "@/components/icons/IconBroadcast";
 import IconCalendar from "@/components/icons/IconCalendar";
 import IconCamera from "@/components/icons/IconCamera";
 import IconCancel from "@/components/icons/IconCancel";
@@ -90,6 +91,8 @@ Vue.component("IconAddUser", IconAddUser);
 Vue.component("IconArrowDown", IconArrowDown);
 Vue.component("IconAutoAnswer", IconAutoAnswer);
 Vue.component("IconAutoReply", IconAutoReply);
+
+Vue.component("IconBroadcast", IconBroadcast);
 
 Vue.component("IconCalendar", IconCalendar);
 Vue.component("IconCamera", IconCamera);
