99121db2202279063845d2ddc455e1ec92028c23
Le Khang <khangle0608@gmail.com>
2019-03-24 19:38:13 +0700

    update file before config error resetpassword

--- client/src/views/customer/broadcast/components/main/schedule/index.script.js
+++ client/src/views/customer/broadcast/components/main/schedule/index.script.js
@@ -2,16 +2,30 @@ import AppHeader from "./components/header";
 import AppDesc from "./components/desc";
 import AppBody from "./components/body";
 
+import StringFunction from "@/utils/string.util";
 export default {
   data() {
-    return {
-      
-    };
+    return {};
   },
   computed: {
     currentTheme() {
       return this.$store.getters.themeName;
+    },
+    schedules() {
+      const dataArr = this.$store.getters.itemBroadcasts;
+      return dataArr.filter(
+        item =>
+          StringFunction.convertUnicode(item.typeBroadCast)
+            .toLowerCase()
+            .trim() === "thiet lap bo hen"
+      );
     }
+  },
+  async created() {
+    await this.$store.dispatch(
+      "getItemBroadcasts",
+      this.$route.params.scheduleId
+    );
   },
   components: {
     AppHeader,
