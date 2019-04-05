373a7c5ac6cfc19e7eab0bb5ccd931ec1d198215
hoc-anms <phamvanhoc96@gmail.com>
2019-04-05 18:02:01 +0700

    update before merge branch dev-toantr-vue

--- server/src/controllers/attribute.controller.js
+++ server/src/controllers/attribute.controller.js
@@ -35,7 +35,7 @@ module.exports = {
 			if (!dataResponse) return res.status(403).json(JsonResponse("Thuộc tính không tồn tại"))
 			dataResponse = dataResponse.map((item) => {
 				if (item._account.toString() === userId) return item
-			})
+			}).filter(item => item !== undefined)
 		} else if (DecodeRole(role, 10) === 1 || DecodeRole(role, 10) === 2) {
 			dataResponse = await Attribute.find(req.query)
 			if (!dataResponse) return res.status(403).json(JsonResponse("Lấy dữ liệu thất bại!", null))
@@ -108,17 +108,19 @@ module.exports = {
 	filter: async (req, res) => {
 		const userId = Secure(res, req.headers.authorization)
 		const accountResult = await Account.findById(userId)
-		const attrResult = await Attribute.find({'_account': userId})
-		if (!attrResult) res.status(403).json(JsonResponse("Thuộc tính này không tồn tại!", null))
 		if (!accountResult) res.status(403).json(JsonResponse("Người dùng không tồn tại!", null))
 		if (req.query._name && !req.query._value && !req.query._type) {
-
+			const foundAttribute = await Attribute.find({'_account':accountResult._id, 'name':req.query._name})
+			if (foundAttribute.length <0) return res.status(403).json(JsonResponse('Không tìm thấy attribue',null))
+			console.log(foundAttribute)
 		}
 		if (req.query._name && req.query._value && req.query._type === 'is') {
-
+			const foundAttribute = await Attribute.find({'_account':accountResult._id, 'name':req.query._name})
+			if (foundAttribute.length <0) return res.status(403).json(JsonResponse('Không tìm thấy attribue',null))
 		}
 		if (req.query._name && req.query._value && req.query._type === 'is not') {
-
+			const foundAttribute = await Attribute.find({'_account':accountResult._id, 'name':req.query._name})
+			if (foundAttribute.length <0) return res.status(403).json(JsonResponse('Không tìm thấy attribue',null))
 		}
 	}
 }
